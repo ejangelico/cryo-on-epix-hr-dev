0f4af1af75eda5b6b8755fb7986b96123d757d8b
Dionisio Doering <ddoering@slac.stanford.edu>
2020-05-12 16:17:55 -0700

    updating tool

--- firmware/setup_template.sh
+++ firmware/setup_template.sh
@@ -1,5 +1,5 @@
 # Setup the Xilinx software and licensing
-source /afs/slac.stanford.edu/g/reseng/xilinx/vivado_2018.2/Vivado/2018.2/settings64.sh
+source /afs/slac.stanford.edu/g/reseng/xilinx/vivado_2019.1/Vivado/2019.1/settings64.sh
 
 # Setup VCS simulation environment 
 source /afs/slac.stanford.edu/g/reseng/synopsys/vcs-mx/N-2017.12-1/settings.sh
